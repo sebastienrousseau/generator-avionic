a5ef4f2b38bee421dbdeffbf8023620722105ed2
Sebastien Rousseau <sebastien.rousseau@mac.com>
2015-04-23 23:49:35 +0100

    Updating ExampleService
    Signed-off-by: Sebastien Rousseau <sebastien.rousseau@mac.com>

--- app/templates/scripts/services/ExampleService.js
+++ app/templates/scripts/services/ExampleService.js
@@ -1,8 +1,9 @@
-'use strict';
+(function () {
+  'use strict';
 
 /**
  * @ngdoc function
- * @name <%= ngModulName %>.serive:ExampleService
+ * @name <%= ngModulName %>.service:ExampleService
  * @description
  * # ExampleService
  */
@@ -10,6 +11,37 @@ angular.module('<%= ngModulName %>')
   // use factory for services
   .factory('ExampleService', function($http, $timeout, $q) {
 
+    // var kindOfPrivateVariable = 42;
+    //
+    // var doSomethingAsync = function() {
+    //   var deferred = $q.defer();
+    //   $timeout(deferred.resolve.bind(null, kindOfPrivateVariable), 1000);
+    //   return deferred.promise;
+    // };
+    //
+    // var fetchSomethingFromServer = function() {
+    //   return $http({
+    //       url: 'http://hipsterjesus.com/api',
+    //       params: {
+    //           paras: 2
+    //       },
+    //       method: 'GET'
+    //     })
+    //     .success(function(data) {
+    //       console.log('fetched this stuff from server:', data);
+    //     })
+    //     .error(function(error) {
+    //       console.log('an error occured', error);
+    //     });
+    // };
+    //
+    // // public api
+    // return {
+    //   doSomethingAsync: doSomethingAsync,
+    //   fetchSomethingFromServer: fetchSomethingFromServer
+    // };
+
+    var endPointUrl = 'http://jsonstub.com/api';
     var kindOfPrivateVariable = 42;
 
     var doSomethingAsync = function() {
@@ -19,19 +51,32 @@ angular.module('<%= ngModulName %>')
     };
 
     var fetchSomethingFromServer = function() {
+      var deferred = $q.defer();
       return $http({
-          url: 'http://hipsterjesus.com/api',
-          params: {
-              paras: 2
-          },
-          method: 'GET'
-        })
-        .success(function(data) {
-          console.log('fetched this stuff from server:', data);
-        })
-        .error(function(error) {
-          console.log('an error occured', error);
-        });
+          url: endPointUrl,
+          method: 'GET',
+          dataType: 'json',
+          data: '',
+          headers: {
+              'Access-Control-Allow-Origin': 'http://jsonstub.com',
+              'Access-Control-Allow-Headers': 'Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With',
+              'Access-Control-Allow-Methods': 'GET',
+              'Content-Type': 'application/json',
+              'JsonStub-User-Key': '0d69f758-3b93-4e27-89ea-959709deb4ba',
+              'JsonStub-Project-Key': 'd6ed0f8c-04bb-495f-a721-5c13b0223bc4'
+          }
+      }).success(function(data, status, headers, config) {
+            console.log('SUCCESS DATA: '+ JSON.stringify(data));
+            console.log('SUCCESS STATUS: '+ JSON.stringify(status));
+            console.log('SUCCESS CONFIG :'+ JSON.stringify(config));
+
+          })
+          .error(function(data, status, headers, config) {
+            console.log('ERROR DATA: '+ JSON.stringify(data));
+            console.log('ERROR STATUS: '+ JSON.stringify(status));
+            console.log('ERROR CONFIG :'+ JSON.stringify(config));
+            return deferred.reject('There was an error fetching objects');
+          });
     };
 
     // public api
@@ -41,3 +86,4 @@ angular.module('<%= ngModulName %>')
     };
 
   });
+})();
